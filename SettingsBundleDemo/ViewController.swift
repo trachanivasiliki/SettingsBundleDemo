f33545eb12f4ea854c6e5489f987646261e8963c
Vasiliki Trachani <trachani.vasiliki@gmail.com>
2021-02-06 19:17:25 +0200

    initialize project

---
+++ SettingsBundleDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingsBundleDemo
+//
+//  Created by Vasiliki Trachani on 6/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
